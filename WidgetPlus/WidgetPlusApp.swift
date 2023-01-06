10f3b547c24e7ecc628b00a5116ee77c77e23666
Louis Aeilot <1329415023@qq.com>
2023-01-06 19:52:20 +0800

    Initial Commit

---
+++ WidgetPlus/WidgetPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetPlusApp.swift
+//  WidgetPlus
+//
+//  Created by Louis Aeilot on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
